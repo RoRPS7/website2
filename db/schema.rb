b69c8f3a0ab3d9b118317856da1cf69bc685793e
YoungEsportPlayer <64225998+YoungEsportPlayer@users.noreply.github.com>
2021-04-24 21:08:51 +0200

    Revert "bootstrap"
    This reverts commit bfaa3bd592b9d143b9269a4f07b25bb111ccc645.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_173355) do
+ActiveRecord::Schema.define(version: 2021_04_24_163156) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -18,11 +18,18 @@ ActiveRecord::Schema.define(version: 2021_04_24_173355) do
     t.float "y"
     t.float "price"
     t.string "description"
-    t.text "time"
-    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_attractions_on_category_id"
+  end
+
+  create_table "attractions_categories", id: false, force: :cascade do |t|
+    t.integer "attraction_id", null: false
+    t.integer "category_id", null: false
+  end
+
+  create_table "attractions_tours", id: false, force: :cascade do |t|
+    t.integer "attraction_id", null: false
+    t.integer "tour_id", null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -33,26 +40,29 @@ ActiveRecord::Schema.define(version: 2021_04_24_173355) do
 
   create_table "discounts", force: :cascade do |t|
     t.string "name"
-    t.integer "percent"
+    t.decimal "percent"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "guides", force: :cascade do |t|
     t.string "name"
-    t.text "surname"
+    t.string "surname"
     t.integer "telnumber"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tours", force: :cascade do |t|
-    t.integer "guide_id", null: false
-    t.integer "attraction_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["attraction_id"], name: "index_tours_on_attraction_id"
+    t.integer "guide_id", null: false
     t.index ["guide_id"], name: "index_tours_on_guide_id"
+  end
+
+  create_table "tours_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "tour_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,7 +76,11 @@ ActiveRecord::Schema.define(version: 2021_04_24_173355) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
-  add_foreign_key "attractions", "categories"
-  add_foreign_key "tours", "attractions"
+  add_foreign_key "attractions_categories", "attractions"
+  add_foreign_key "attractions_categories", "categories"
+  add_foreign_key "attractions_tours", "attractions"
+  add_foreign_key "attractions_tours", "tours"
   add_foreign_key "tours", "guides"
+  add_foreign_key "tours_users", "tours"
+  add_foreign_key "tours_users", "users"
 end
