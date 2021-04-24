2c5590f92df9706e23b46730a85621edc7797242
Arkadiusz Abramowicz <areek99@gmail.com>
2021-04-24 21:53:05 +0200

    Podgląd tras

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,4 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_and_belongs_to_many :tours
-
-  validates :login, presence: true
-  validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_163156) do
+ActiveRecord::Schema.define(version: 2021_04_24_190138) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_04_24_163156) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "time"
   end
 
   create_table "attractions_categories", id: false, force: :cascade do |t|
