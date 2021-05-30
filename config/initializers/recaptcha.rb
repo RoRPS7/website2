1ee5beefe21a80c3eb3ee8b018f663db56de24b4
Arkadiusz Abramowicz <areek99@gmail.com>
2021-05-30 18:13:59 +0200

    captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6Lc-IcoaAAAAAEZRb05ybASDq1gCOAtMtB947dlJ'
-  config.secret_key = '6Lc-IcoaAAAAAIpNA5IPZ4nv_jFGCJyieO74LUTY'
+  config.site_key  = '6LeBiv8aAAAAACuAtFILse1j0y780pXxU3fdBThU'
+  config.secret_key = '6LeBiv8aAAAAALUVq36_4gMR6N8H-DgQMHqdKK_o'
 end
