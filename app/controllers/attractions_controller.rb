9ec918271a984b8942808d42ec4bb555f13562a9
ondreo <65464616+ondreo@users.noreply.github.com>
2021-06-15 22:26:50 +0200

    usunięcie zbędnych odwołań do edycji itd. z poziomu zwykłej strony (a nie panelu admina) oraz usunięcie starego, pozostałego testu

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -13,48 +13,27 @@ class AttractionsController < ApplicationController
 
   # GET /attractions/new
   def new
-    @attraction = Attraction.new
+    render plain: "404 Not Found"
   end
 
   # GET /attractions/1/edit
   def edit
+    render plain: "404 Not Found"
   end
 
   # POST /attractions or /attractions.json
   def create
-    @attraction = Attraction.new(attraction_params)
-
-    respond_to do |format|
-      if @attraction.save
-        format.html { redirect_to @attraction, notice: "Attraction was successfully created." }
-        format.json { render :show, status: :created, location: @attraction }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @attraction.errors, status: :unprocessable_entity }
-      end
-    end
+    render plain: "404 Not Found"
   end
 
   # PATCH/PUT /attractions/1 or /attractions/1.json
   def update
-    respond_to do |format|
-      if @attraction.update(attraction_params)
-        format.html { redirect_to @attraction, notice: "Attraction was successfully updated." }
-        # format.json { render :show, status: :ok, location: @attraction }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @attraction.errors, status: :unprocessable_entity }
-      end
-    end
+    render plain: "404 Not Found"
   end
 
   # DELETE /attractions/1 or /attractions/1.json
   def destroy
-    @attraction.destroy
-    respond_to do |format|
-      format.html { redirect_to attractions_url, notice: "Attraction was successfully destroyed." }
-      # format.json { head :no_content }
-    end
+    render plain: "404 Not Found"
   end
 
   private
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,52 +8,32 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1 or /categories/1.json
   def show
+    render plain: "404 Not Found"
   end
 
   # GET /categories/new
   def new
-    @category = Category.new
+    render plain: "404 Not Found"
   end
 
   # GET /categories/1/edit
   def edit
+    render plain: "404 Not Found"
   end
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
+    render plain: "404 Not Found"
   end
 
   # PATCH/PUT /categories/1 or /categories/1.json
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
+    render plain: "404 Not Found"
   end
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    render plain: "404 Not Found"
   end
 
   private
