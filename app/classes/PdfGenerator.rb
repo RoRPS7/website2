16ced05ca2e2b737c6cb9055cbed66f5db937c08
Arkadiusz Abramowicz <areek99@gmail.com>
2021-06-16 00:41:47 +0200

    Trasy - pdf i wyglad

--- app/classes/PdfGenerator.rb
+++ app/classes/PdfGenerator.rb
@@ -1,7 +1,5 @@
-
-
 class PdfGenerator < Prawn::Document
-  def initialize(tours,instruction)
+  def initialize(tours,instruction,time, distance)
     super()
     font_families.update("Roboto" => {
       :normal => Rails.root.join("app/assets/fonts/Roboto-Black.ttf"),
@@ -9,31 +7,43 @@ class PdfGenerator < Prawn::Document
     font "Roboto"
 
     font_size(24) do
-      text "\n\n\n bruh\n\n\n", :align => :center
-      text instruction , :align => :center
+      text "\n\n\n BialystokRoutes - Trasa przejścia\n\n\n", :align => :center
     end
-
+    
+    
     image "#{Rails.root}/app/assets/images/logo.png", :at => [240,730], :width => 65
-    #table [["Nazwa obiektu:","Oplata:","Czas Zwiedzania:"]] do
-    #  columns(0).width =200vvv
-    #  columns(1).width =70
-    #  columns(2).width =110
-    #  style(row(0), :background_color => 'FFCF7F')vvvvvvvvvvvvv
-    #end
 
-    tours.attractions.each do |attraction|
-        font_size(16) do
-          text attraction.name + "\n\n", :align => :center
-        end
-        text attraction.description + "\n\n\n"
+      font_size(16) do
+        text "Atrakcje na trasie" + "\n\n", :align => :center
+      end
+      
+      @tour = Attraction.find(tours.attraction_start_id).name
+      text "Atrakcja początkowa: "+ @tour + "\n\nAtrakcje przejściowe: \n"
+      
+      tours.attractions.each do |attraction|
+        text attraction.name
+      end
 
-      #data =[[attractions.attractions.name,attractions.id,"nuts"]]
-      #table data do
-      #  columns(0).width =200
-      #  columns(1).width =70
-      #  columns(2).width =110
-      #end
+      @tour = Attraction.find(tours.attraction_end_id).name
+      text "Atrakcja końcowa: "+ @tour + "\n\n"
+
+    font_size(16) do
+      text "Informacje dodatkowe:\n"
     end
+    text "Całkowite opłaty: " + tours.sumcosts.to_s + "zł"
+
+    if !tours.discount_id.nil?
+      tmp = tours.sumcosts - (tours.sumcosts * (1/Discount.find(tours.discount_id).percent)).round(2)
+      tmp = tmp.to_s
+      text "Całkowita opłata ze zniżką: " + tmp +" zł."
+    end
+
+      text time
+      text distance
+    font_size(16) do
+       text "\n\nInstrukcja przejscia:", :align => :center
+    end
+    text instruction
   end
 end
 
