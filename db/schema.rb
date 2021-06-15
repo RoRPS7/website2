4df9174289ae96fbb8a19a2e0e8fa1dfc5750ffa
Arkadiusz Abramowicz <areek99@gmail.com>
2021-06-15 18:25:10 +0200

    Dodanie zdjęcia i opisu do przewodnika

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_095741) do
+ActiveRecord::Schema.define(version: 2021_06_15_162401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_095741) do
     t.integer "telnumber"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "tours", force: :cascade do |t|
