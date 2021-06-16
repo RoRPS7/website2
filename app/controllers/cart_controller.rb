972d98f42159180dc57a61d1aa70bf04550c524e
Arkadiusz Abramowicz <areek99@gmail.com>
2021-06-16 10:05:56 +0200

    cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -29,7 +29,7 @@ class CartController < ApplicationController
       @tour.totaltime =  @tmp.sightseeing_time
       @tour.sumcosts += @tmp.price
     end
-    @cart.without(@tour.attraction_end_id, @tour.attraction_start_id).each do |attraction|
+    Attraction.where(id: @tour.attraction_ids).each do |attraction|
       @tour.totaltime += attraction.sightseeing_time
       @tour.sumcosts += attraction.price
     end
